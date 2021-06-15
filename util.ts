f0e2f317fe823932968d8985bee279d3d9579255
Amos Decker <amdecker9@gmail.com>
2021-06-15 10:36:25 -0400

    more sanitization

--- util.ts
+++ util.ts
@@ -17,14 +17,13 @@ export const doArgdownProcessing = async (fileContents:string, nameFile: string)
 	// I add the slash in at createTmpDir because its easier to take it out than to add it in everywhere
 	const pathWithoutTrailingSlash = pathToTmpDir.substring(0, pathToTmpDir.length - 1);
 
-	let command = `export PATH="$PATH:"/usr/local/bin/; argdown web-component '${pathToTmpDir.replaceAll('"', "\"").replaceAll("'", "\'\"\'\"\'")}${nameFile.replaceAll('"', "\"").replaceAll("'", "\'\"\'\"\'")}.md' '${pathWithoutTrailingSlash.replaceAll('"', "\"").replaceAll("'", "\'\"\'\"\'")}'`
+	let command = `export PATH="$PATH:"/usr/local/bin/; argdown web-component '${sanitize(pathToTmpDir + nameFile)}.md' '${sanitize(pathWithoutTrailingSlash)}'`;
 	if(isWin) {
-		command = `argdown web-component "${pathToTmpDir}${nameFile}.md" "${pathWithoutTrailingSlash}"`
+		command = `argdown web-component "${sanitize(pathToTmpDir + nameFile)}.md" "${sanitize(pathWithoutTrailingSlash)}"`
 	}
 	await runCmd(command);
 	return `${pathToTmpDir}${nameFile}.component.html`.replaceAll('"', "%22");
 };
-
 
 export const createTempMarkdownFile = async (fileContent:string, nameFile: string) => {
 	let path = pathToTmpDir + nameFile + ".md";
@@ -73,15 +72,25 @@ export const deleteTmpDir = () => {
 };
 
 export const deleteTmpFiles = async (nameFile: string) => {
-	let command = `rm "${pathToTmpDir}${nameFile}.md"`;
+	let command = `rm "${sanitize(pathToTmpDir + nameFile)}.md"`;
 	if(isWin) {
-		command = `del "${pathToTmpDir}${nameFile}.md"`;
+		command = `del "${sanitize(pathToTmpDir + nameFile)}.md"`;
 	}
 	await runCmd(command);
 
-	command = `rm "${pathToTmpDir}${nameFile}.component.html"`;
+	command = `rm "${sanitize(pathToTmpDir + nameFile)}.component.html"`;
 	if(isWin) {
-		command = `del "${pathToTmpDir}${nameFile}.component.html"`;
+		command = `del "${sanitize(pathToTmpDir + nameFile)}.component.html"`;
 	}
 	await runCmd(command);
+};
+
+
+const sanitize = (command:string) => {
+	return command.replaceAll(/[ !"#$&'()*;<>?\[\\`{|~\t\n]/g, (match:string) => {
+		if(match === "'") {
+			return "\'\"\'\"\'";
+		}
+		return "\\" + match;
+	})
 };
